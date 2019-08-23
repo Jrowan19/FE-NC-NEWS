a8814fc247cc18e829583faf554c86b792e26e84
john rowan <johnrowan@johns-MacBook-Air.local>
2019-08-23 09:22:16 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -3,7 +3,11 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return <div className="App" />;
+  return (
+    <div className="App">
+      ;<h1>hello</h1>;
+    </div>
+  );
 }
 
 export default App;

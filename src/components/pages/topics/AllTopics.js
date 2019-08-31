aff913d87674998135d550c69a412b6e5b1fe5c5
john rowan <johnrowan@johns-air.home>
2019-08-31 10:32:42 +0100

    made article boxes flex

--- src/components/pages/topics/AllTopics.js
+++ src/components/pages/topics/AllTopics.js
@@ -16,30 +16,54 @@ class AllTopics extends Component {
     if (error) return <ErrorMsg error={error} />;
     if (isLoading) return <LoadingSpinner />;
     return (
-      <div>
+      <div className="row mx-auto sitewrapper">
         {topics.map(topic => {
           return (
-            <div
-              className="card mx-auto bg-dark mb-5 mt-3"
-              style={{ width: '50rem' }}
-            >
-              <img
-                src="https://thumbs.dreamstime.com/t/header-banner-3589701.jpg"
-                className="card-img-top"
-                style={({ width: '50rem' }, { height: '10rem' })}
-                alt="..."
-              />
-              <div class="card-body">
-                <Link to={`/topics/${topic.slug}`}>
-                  <h5 className="card-title text-uppercase">
-                    {topic.slug} Articles
-                  </h5>
-                </Link>
-                <Link to={`/topics/${topic.slug}`}>
-                  <button className="btn btn-primary">View Articles</button>
-                </Link>
-              </div>
-            </div>
+            <ul className=" row card mb-3 mx-auto" style={{ width: '540px' }}>
+              <li className="row no-gutters">
+                <div class="col-md-4">
+                  <img
+                    src="https://thumbs.dreamstime.com/t/header-banner-3589701.jpg"
+                    className="card img-fluid"
+                    style={{ height: '10rem' }}
+                    alt="..."
+                  />
+                </div>
+                <div class="col-md-8">
+                  <div class="card-body">
+                    <Link to={`/topics/${topic.slug}`}>
+                      <h5 class="card-title">{topic.slug} Articles</h5>
+                    </Link>
+                    <p class="card-text">
+                      This is a wider card with supporting text below as a
+                      natural lead-in to additional content. This content is a
+                      little bit longer.
+                    </p>
+                    <p class="card-text">
+                      <small class="text-muted">Last updated 3 mins ago</small>
+                    </p>
+                  </div>
+                </div>
+              </li>
+            </ul>
+            // <div className=" card-forkrow mx-auto" style={{ width: '50rem' }}>
+            //   <img
+            //     src="https://thumbs.dreamstime.com/t/header-banner-3589701.jpg"
+            //     className="card img-fluid"
+            //     style={{ height: '10rem' }}
+            //     alt="..."
+            //   />
+            //   <div class="card-body ">
+            //     <Link to={`/topics/${topic.slug}`}>
+            //       <h5 className="card-title text-uppercase">
+            //         {topic.slug} Articles
+            //       </h5>
+            //     </Link>
+            //     <Link to={`/topics/${topic.slug}`}>
+            //       <button className="btn btn-primary">View Articles</button>
+            //     </Link>
+            //   </div>
+            // </div>
           );
         })}
       </div>
